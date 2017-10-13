294c05c16e0719381da01ddd0965cda54c88f420
laverix <valery.lyatsevich@gmail.com>
2017-10-13 15:27:22 +0300

    Making a big move to Angular4

--- app/js/filters/perc.js
+++
@@ -1,5 +0,0 @@
-angular.module('mrmagnet').filter('percentage', ['$filter', function ($filter) {
-    return function (input, decimals) {
-        return $filter('number')(input * 100, decimals) + '%';
-    };
-}]);
--- main.js
+++ main.js
@@ -1,19 +1,35 @@
-'use strict';
-const {app} = require('electron');
-const {BrowserWindow} = require('electron');
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({width: 800, height: 300});
+
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'dist/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  win.webContents.openDevTools();
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
     app.quit()
+  }
 });
 
-app.on('ready', function () {
-    let win = new BrowserWindow({width: 800, height: 300, resizable: true});
-
-    win.on('closed', () => {
-        win = null
-    });
-
-    win.loadURL('file://' + __dirname + '/app/index.html');
-
-    win.openDevTools();
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
 });
